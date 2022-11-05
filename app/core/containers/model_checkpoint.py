4308919e4e00051785895bae759fd1703b28f4a4
martinHwang <dhhwang89@gmail.com>
2022-11-05 13:46:48 +0900

    fix: 누락된 코드 추가
     Refs:

--- app/core/containers/model_checkpoint.py
+++ app/core/containers/model_checkpoint.py
@@ -1,16 +1,9 @@
 from dependency_injector import containers, providers
-from pytorch_lightning.callbacks import ModelCheckpoint
 
 from ...core.patterns.registry import ModelCheckpointRegistry
 
 
-def register_default_modelcheckpoint():
-    ModelCheckpointRegistry.register(ModelCheckpoint)
-
-
 class ModelCheckpoint(containers.DeclarativeContainer):
-    register_default_modelcheckpoint()
-
     config = providers.Configuration()
     instance = ModelCheckpointRegistry.get(config.name)
     checkpoint = providers.Singleton(
